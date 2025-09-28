0c5e9fdd1fb72fe0b149287d31a8648f1baf6352
masafirtester1-beep <masafir.tester1@gmail.com>
2025-09-28 20:33:34 +0100

    Delete app/src/main/app.js

--- app/src/main/app.js
+++
@@ -1,7 +0,0 @@
-function openRouter() {
-  // هاد العنوان غادي يتبدل بالـ IP ديال الراوتر ديالك
-  window.location.href = "http://192.168.1.1";
-}
-
-// رسالة ترحيب فـ الكونسول باش نتاكد بلّي JavaScript خدام
-console.log("✅ app.js loaded successfully!");
