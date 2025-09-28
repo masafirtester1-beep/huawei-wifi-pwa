205be09ad206c8cf648d35afc3d1ba0bd3e652b0
masafirtester1-beep <masafir.tester1@gmail.com>
2025-09-28 22:50:25 +0100

    Delete app directory

--- app/src/main/app/src/main/assets/app/src/main/assets/app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("open").addEventListener("click", () => {
-  const gw = document.getElementById("gw").value.trim();
-  const u  = document.getElementById("user").value.trim();
-  const p  = document.getElementById("pass").value.trim();
-
-  // حاليا يفتح صفحة الراوتر في المتصفح
-  const url = gw || "http://192.168.8.1";
-  window.location.href = url;
-});
