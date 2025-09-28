602ce19b065fa07fdda2fbcce5a7aa0d353d18a5
masafirtester1-beep <masafir.tester1@gmail.com>
2025-09-28 20:15:45 +0100

    Create app.js

---
+++ app/src/main/app.js
@@ -0,0 +1,7 @@
+function openRouter() {
+  // هاد العنوان غادي يتبدل بالـ IP ديال الراوتر ديالك
+  window.location.href = "http://192.168.1.1";
+}
+
+// رسالة ترحيب فـ الكونسول باش نتاكد بلّي JavaScript خدام
+console.log("✅ app.js loaded successfully!");
