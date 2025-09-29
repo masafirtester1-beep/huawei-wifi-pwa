2ce27504cf0192a2df3950de11faa65b9d77f76f
masafirtester1-beep <masafir.tester1@gmail.com>
2025-09-29 23:11:24 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/huaweiwifi/MainActivity.kt
+++ app/src/main/java/com/example/huaweiwifi/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.wifi.WifiInfo
-import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
