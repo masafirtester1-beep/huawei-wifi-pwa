9683b51af82871ffd990422d2fe70da3e5b7d779
masafirtester1-beep <masafir.tester1@gmail.com>
2025-09-29 16:06:10 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,17 +26,25 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.8.0")
+
+    // ✅ OkHttp
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    // (اختياري) باش تسهّل التعامل مع JSON فالـ OkHttp responses
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
