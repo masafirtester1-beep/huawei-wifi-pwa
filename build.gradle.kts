02fb7a06fbe9586542c863026a3da8f765dbcdad
masafirtester1-beep <masafir.tester1@gmail.com>
2025-09-29 09:04:59 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    // These are version catalogs for plugins; app module will apply them
+    id("com.android.application") version "8.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
